72328020590e64657b7550ae7f929e0196c29a55
Senad Zukorlic <zukorlic.senad01@gmail.com>
2025-10-13 12:56:15 +0200

    FirstProgram

---
+++ src/WorkBooks/FirstProgram.java
@@ -0,0 +1,15 @@
+package WorkBooks;
+// I'm a comment. I have no impact on your code.
+// Instructions for this workbook are on Learn the Part (See the Udemy Video: Workbook 1.1 to access the link).
+
+public class FirstProgram {
+    public static void main(String[]args){
+        System.out.println(" SSS           ZZZZZZZ");
+        System.out.println("S   S             ZZ");
+        System.out.println("S                ZZ");
+        System.out.println(" SSS            ZZ");
+        System.out.println("    S          ZZ");
+        System.out.println("S   S         ZZ");
+        System.out.println(" SSS         ZZZZZZZZ");
+    }
+}
